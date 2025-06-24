11228c8da1d50f011f232c1412c529729f0db750
galchacoa2003 <galchacoa.4347@unimar.edu.ve>
2025-06-24 18:09:13 -0400

    commi add - actualizacion ultima bdd con la data

--- comentario.sql
+++ comentario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-06-2025 a las 23:11:04
+-- Tiempo de generación: 25-06-2025 a las 00:08:08
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -36,6 +36,14 @@ CREATE TABLE `comentario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Volcado de datos para la tabla `comentario`
+--
+
+INSERT INTO `comentario` (`id`, `nombreyapellido`, `email`, `nota`, `fechanota`) VALUES
+(8, 'Soichiro Honda', 'soichirohonda@gmail.com', 'Me alegra saber que elegiste un Honda Civic. Es un auto que refleja nuestra filosofía de innovación y calidad accesible. Espero que disfrutes cada viaje. ¡Gracias por confiar en la marca!', '2025-06-24 23:55:34'),
+(9, 'Post Malone', 'postmalone@gmail.com', 'Yo, este carro se ve increíble. No puedo creer lo bien que quedó. Definitivamente estoy en buena onda con él.', '2025-06-24 23:59:40');
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -53,7 +61,7 @@ ALTER TABLE `comentario`
 -- AUTO_INCREMENT de la tabla `comentario`
 --
 ALTER TABLE `comentario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
